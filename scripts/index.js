0fdaa47ddbb3b218d99af4564711d22293c627d4
Rami B <ramibasheer9@gmail.com>
2022-10-08 22:32:03 +0200

    apply solution

--- scripts/index.js
+++ scripts/index.js
@@ -2,4 +2,22 @@ import View from './View.js';
 import Client from './Client.js';
 
 // All of your javascript should go here
-console.log("Hello from index.js");
+
+const input = document.querySelector("input");
+
+const newClient = new Client();
+const newView = new View();
+
+
+
+
+
+
+input.addEventListener("change", async () => {
+    if(input.value) {
+    const data = await newClient.getMovieData(input.value);
+    console.log(data);
+
+    newView.displayMovieOnPage(data);
+    }
+})
